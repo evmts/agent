ab1d7303e6b58b4cc262b7276679977065823333
William Cory <willcory10@gmail.com>
2025-06-13 21:39:34 -0700

    chore: Remove unused code

--- build.zig
+++ build.zig
@@ -42,22 +42,6 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // We will also create a module for our other entry point, 'main.zig'.
-    const exe_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
-    exe_mod.addImport("plue_lib", lib_mod);
-
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
     // for actually invoking the compiler.
@@ -99,36 +83,6 @@ pub fn build(b: *std.Build) void {
         .verbose = .err,
     });
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
-    const exe = b.addExecutable(.{
-        .name = "plue",
-        .root_module = exe_mod,
-    });
-    exe.linkLibrary(webui.artifact("webui"));
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
@@ -136,12 +90,6 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Integration tests for our Zig modules
     const integration_tests = b.addTest(.{
@@ -161,7 +109,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     libplue_test_mod.addImport("libplue", c_lib_mod);
-    
+
     const libplue_tests = b.addTest(.{
         .root_module = libplue_test_mod,
     });
@@ -172,7 +120,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     farcaster_test_mod.addImport("farcaster", farcaster_mod);
-    
+
     const farcaster_tests = b.addTest(.{
         .root_module = farcaster_test_mod,
     });
@@ -183,7 +131,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     app_test_mod.addImport("app_root", lib_mod);
-    
+
     const app_tests = b.addTest(.{
         .root_module = app_test_mod,
     });
@@ -197,7 +145,6 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
     test_step.dependOn(&run_integration_tests.step);
     test_step.dependOn(&run_libplue_tests.step);
     test_step.dependOn(&run_farcaster_tests.step);
@@ -218,34 +165,32 @@ pub fn build(b: *std.Build) void {
 
     // Add Swift build step that depends on Zig libraries
     const swift_build_cmd = b.addSystemCommand(&.{
-        "swift", "build", "--configuration", "release",
+        "swift",    "build",                                       "--configuration", "release",
         "-Xlinker", b.fmt("-L{s}", .{b.getInstallPath(.lib, "")}),
     });
-    
+
     // Swift build depends on all Zig libraries being built and installed
     swift_build_cmd.step.dependOn(&lib.step);
     swift_build_cmd.step.dependOn(&c_lib.step);
     swift_build_cmd.step.dependOn(&farcaster_lib.step);
-    
+
     // Create a step for building the complete project (Zig + Swift)
     const build_all_step = b.step("swift", "Build complete project including Swift");
     build_all_step.dependOn(&swift_build_cmd.step);
-    
+
     // Make the default install step also build Swift
     b.getInstallStep().dependOn(&swift_build_cmd.step);
-    
+
     // Add step to run the Swift executable
-    const swift_run_cmd = b.addSystemCommand(&.{
-        ".build/release/plue"
-    });
+    const swift_run_cmd = b.addSystemCommand(&.{".build/release/plue"});
     swift_run_cmd.step.dependOn(&swift_build_cmd.step);
-    
+
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the Swift app");
     run_step.dependOn(&swift_run_cmd.step);
-    
+
     const run_swift_step = b.step("run-swift", "Run the Swift application");
     run_swift_step.dependOn(&swift_run_cmd.step);
 }
